b7af51b6b16a63c514194d763eece2c4dc9de7f9
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-04-30 20:18:45 +0200

    111

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Feedback from './Feedback/FeedbackOption';
+import './App.scss'
+import { Component } from 'react';
+
+
+class App extends Component {
+  state = { 
+  good: 0,
+  neutral: 0,
+  bad: 0,
+}
+
+}
+export default App;
+  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+//import Notiflix from 'notiflix';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import reportWebVitals from './reportWebVitals';
+ import App  from './components/App';
+import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+//reportWebVitals();
